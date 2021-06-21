03b144479c56329aab46e96200f65f6f5976bc71
ChenDeli <wb-cdl392269@alibaba-inc.com>
2021-06-21 18:28:27 +0800

    更新打包配置

--- webpack.common.js
+++ webpack.common.js
@@ -1,95 +1,100 @@
 const path = require("path");
 const webpack = require("webpack");
 const { VueLoaderPlugin } = require('vue-loader')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        ),
-        use: [
-          // "thread-loader",// 多线程编译，可能会导致报错
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        ],
-      },
-
-      {
-        test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: ['vue-style-loader', 'css-loader', 'less-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'fonts/'
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'assets/'
-          }
-        }
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          use: [
-            // "thread-loader",// 多线程编译，可能会导致报错
+    entry: "./src/index.js",
+    module: {
+        rules: [
             {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          ],
-        }
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['\*', '.js', '.jsx', '.vue'],// 能够使用户在引入模块时不带扩展
-    alias: {
-      "@": path.resolve(__dirname, 'src'),
+                test: /\.js$/,
+                exclude: file => (
+                    /node_modules/.test(file) &&
+                    !/\.vue\.js/.test(file)
+                ),
+                use: [
+                    // "thread-loader",// 多线程编译，可能会导致报错
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ],
+            },
+
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['vue-style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts/'
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets/'
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    use: [
+                        // "thread-loader",// 多线程编译，可能会导致报错
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
+                    ],
+                }
+            },
+        ]
     },
-  },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  //why:https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
-  plugins: [new webpack.HotModuleReplacementPlugin(), new VueLoaderPlugin(),
-  new webpack.DefinePlugin({
-    "__VUE_OPTIONS_API__": true,
-    "__VUE_PROD_DEVTOOLS__": false,
-  })]
+    resolve: {
+        extensions: ['\*', '.js', '.jsx', '.vue'],// 能够使用户在引入模块时不带扩展
+        alias: {
+            "@": path.resolve(__dirname, 'src'),
+        },
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        filename: "bundle.js"
+    },
+    //why:https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            title: `Vue Starter`,
+            favicon: path.resolve(__dirname, "public/favicon.ico"),
+            template: path.resolve(__dirname, "public/index.html")
+        }),
+        new webpack.DefinePlugin({
+            "__VUE_OPTIONS_API__": true,
+            "__VUE_PROD_DEVTOOLS__": false,
+        })]
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,17 @@
 const path = require("path");
 const { merge } = require('webpack-merge');
+const webpack = require("webpack");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public"),
+    contentBase: path.join(__dirname, "dist"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
     hot: true,
     open: true, //自动打开浏览器
     // host: '0.0.0.0',//开启项目本地服务
   },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 });
