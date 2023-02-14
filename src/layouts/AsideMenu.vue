06f0f8109f86c3938a45634588d2b39f6572b3c0
chendeli <419287484@qq.com>
2023-02-14 11:34:55 +0800

    增加一些简单的路由操作

--- src/layouts/AsideMenu.vue
+++ src/layouts/AsideMenu.vue
@@ -1,19 +1,25 @@
 <!--
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2023-02-13 17:02:16
- * @LastEditTime: 2023-02-13 17:20:05
+ * @LastEditTime: 2023-02-14 11:33:41
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 
  * @FilePath: \VuePureFrame\src\layouts\AsideMenu.vue
  * 
 -->
-<template>导航1</template>
+<template>
+  <router-link to="/home">HOME</router-link>
+  <br />
+  <router-link to="/colorlibrary">colorlibrary</router-link>
+</template>
 
 <script>
+import { RouterLink } from "vue-router";
 export default {
   props: {
-    activeIndex: String,
+    // 如果使用 TypeScript，请添加 @ts-ignore
+    ...RouterLink.props,
   },
   methods: {
     handleOpen(key, keyPath) {
