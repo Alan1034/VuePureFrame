78e896961509db1bcaaba8d05160cd4c9e0e39f2
chendeli <419287484@qq.com>
2023-08-14 19:29:18 +0800

    demo

---
+++ dist/js/Home-chunk-5a7d976.js
@@ -0,0 +1,1 @@
+import o from"./MainLayout-chunk-2aaa7ec.js";import{_ as a}from"./index-app-3f5becc.js";import{r as n,o as t,c as r,w as s,e}from"./vendor-chunk-4d2ba2f.js";const u=a({components:{MainLayout:o}},[["render",function(o,a,u,m,i,c){const p=n("main-layout");return t(),r(p,null,{default:s((()=>[e(" Home ")])),_:1})}]]);export{u as default};
