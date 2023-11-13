54354c5df467013fdd583be3d742f9d3840cd27b
Chendeli <419287484@qq.com>
2023-11-13 09:59:20 +0800

    更新eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,10 +17,10 @@ module.exports = {
   },
   'rules': {
     // 添加组件命名忽略规则
-    "vue/multi-word-component-names": ["error",{
-      "ignores": ["index","main"]//需要忽略的组件名
-   }]
-},
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["index", "main"]//需要忽略的组件名
+    }]
+  },
   //   'linebreak-style': [
   //     'error',
   //     'unix'
