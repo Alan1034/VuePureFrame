efdbdb132e7e7808a0dfd5423c13b8fde650cedc
Chendeli <419287484@qq.com>
2023-11-17 17:49:22 +0800

    正确配置环境变量的类型定义

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,18 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2023-11-15 15:49:59
+ * @LastEditTime: 2023-11-17 17:48:20
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \VuePureFrame\env.d.ts
+ * 
+ */
 /// <reference types="vite/client" />
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
