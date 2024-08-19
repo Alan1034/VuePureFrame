2efe476d52187c78c9fbc931236d9a56f6afdb0f
Chendeli <419287484@qq.com>
2024-08-19 10:40:27 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import legacy from '@vitejs/plugin-legacy'
 import { fileURLToPath } from 'url'
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { compression } from 'vite-plugin-compression2'
@@ -59,15 +59,6 @@ export default defineConfig(({ command, mode }) => {
        */
       {
         ...updateVersion(__dirnameNew),
-        apply: 'build',
-      },
-
-      /**
-       * @description: vite自带的文件分割配置
-       * @return {*}
-       */
-      {
-        ...splitVendorChunkPlugin(),
         apply: 'build',
       },
       /**
