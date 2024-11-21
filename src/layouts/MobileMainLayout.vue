06e87939a99882163098ec2e606e4f64777e7b34
Chendeli <419287484@qq.com>
2024-11-21 18:16:36 +0800

    更新

---
+++ src/layouts/MobileMainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-content" :style="{ height: mainHeight + 'px' }">
+    <div class="main-slot" :style="{ height: mainHeight - 48 + 'px' }">
+      <slot />
+    </div>
+
+  </div>
+  <!-- flex-header -->
+  <header-menu />
+</template>
+
+<script>
+import HeaderMenu from "./HeaderMenu.vue";
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      mainHeight: window.innerHeight,
+    };
+  },
+  name: "MainLayout",
+  components: { HeaderMenu },
+};
+</script>
+
+<style lang="less" scoped>
+.main-content {
+  overflow-y: auto;
+
+  .main-slot {
+    margin-top: 48px;
+  }
+}
+</style>
