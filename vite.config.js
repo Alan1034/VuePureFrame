48cfaf39b45783eab8b4eba4842a89965a9bfaad
Chendeli <419287484@qq.com>
2025-11-17 09:50:34 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -53,8 +53,7 @@ export default defineConfig(({ command, mode }) => {
         // 'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
         'vue': 'vue/dist/vue.esm-bundler.js',
         '@': resolve('src'),//路径化名
-      },
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+      }
     },
 
     plugins: [
